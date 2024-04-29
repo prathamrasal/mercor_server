61565c733fffcb8fca9f3eb926fb33c21112ef48
prathamrasal <20bcs038@ietdavv.edu.in>
2024-04-29 16:00:13 +0530

    Fix

--- app/config/constants.py
+++ app/config/constants.py
@@ -1,5 +1,5 @@
 class SECRETS:
-    OPEN_API_KEY="sk-proj-8LKb7WnhjlsxDFJ1sqoVT3BlbkFJZourqVG5uoewGN5DvS60"
-    PINCONE_API_KEY="4bf5ff47-b90a-43e8-b877-b54628d4f978"
+    OPEN_API_KEY="KEY"
+    PINCONE_API_KEY="KEY"
     PINECONE_ENV="gcp-starter"
     PINECONE_INDEX = "engineer-embeddings"
